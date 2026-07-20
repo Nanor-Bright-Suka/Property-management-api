f5058748a6847800900af49d116ac59a3b8433ad
Nanor Bright Suka <brightnanor19@gmail.com>
2026-07-20 12:03:32 +0000

    WIP: implemented 3 integration test cases for the request otp route of the auth controller

--- src/test/java/com/backend/hotelreservationapi/auth_module/controller/AuthControllerTest.java
+++ src/test/java/com/backend/hotelreservationapi/auth_module/controller/AuthControllerTest.java
@@ -6,11 +6,10 @@ import com.backend.hotelreservationapi.auth_module.entity.OtpSession;
 import com.backend.hotelreservationapi.auth_module.enums.OtpRequestStatusEnum;
 import com.backend.hotelreservationapi.auth_module.repository.OtpRequestLogRepository;
 import com.backend.hotelreservationapi.auth_module.service.EmailService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
@@ -20,7 +19,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -42,6 +43,7 @@ class AuthControllerTest extends IntegrationTestBase {
 
 
     @Test
+    @DisplayName(value = "This test test the request otp controller")
     void shouldRequestOtpSuccessfully() throws Exception {
 
         EmailRequestDto request = new EmailRequestDto();
@@ -79,6 +81,120 @@ class AuthControllerTest extends IntegrationTestBase {
         assertThat(sentOtp).hasSize(6);
 
     }
+
+
+    @Test
+    @DisplayName(value = "This test blocks request when email rate limit is exceeded")
+    void shouldBlockRequestWhenEmailRateLimitExceeded() throws Exception {
+
+        EmailRequestDto request = new EmailRequestDto();
+        request.setEmail("john@gmail.com");
+
+        String requestBody = mapper.writeValueAsString(request);
+
+        for (int i = 0; i < 3; i++) {
+
+            mockMvc.perform(post("/api/v1/auth/request-otp")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody))
+                    .andExpect(status().isOk());
+        }
+
+        mockMvc.perform(post("/api/v1/auth/request-otp")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isTooManyRequests())
+                .andExpect(jsonPath("$.message").value("Too many requests from this email"));
+
+        List<OtpRequestLog> logs = otpRequestLogRepository.findAll();
+
+        assertThat(logs).hasSize(4);
+
+        OtpRequestLog lastLog = logs.getLast();
+
+        assertThat(lastLog.getStatus()).isEqualTo(OtpRequestStatusEnum.BLOCKED);
+        assertThat(lastLog.getReason()).isEqualTo("Too many requests from this email");
+
+        verify(emailService, times(3)).sendOtpEmailAsync(eq("john@gmail.com"), anyString());
+    }
+
+
+
+    @Test
+    void shouldBlockRequestWhenIpRateLimitExceeded() throws Exception {
+
+        String ipAddress = "192.168.1.100";
+
+        for (int i = 1; i <= 10; i++) {
+            EmailRequestDto request = new EmailRequestDto();
+            request.setEmail("user" + i + "@gmail.com");
+
+            String requestBody = mapper.writeValueAsString(request);
+
+            mockMvc.perform(post("/api/v1/auth/request-otp")
+                            .header("X-Forwarded-For", ipAddress)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody))
+                    .andExpect(status().isOk());
+        }
+
+
+        EmailRequestDto blockedRequest = new EmailRequestDto();
+        blockedRequest.setEmail("user11@gmail.com");
+
+        String blockRequestBody = mapper.writeValueAsString(blockedRequest);
+
+        mockMvc.perform(post("/api/v1/auth/request-otp")
+                        .header("X-Forwarded-For", ipAddress)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(blockRequestBody))
+                .andExpect(status().isTooManyRequests())
+                .andExpect(jsonPath("$.message")
+                        .value("Too many requests from this IP"));
+
+        List<OtpRequestLog> logs = otpRequestLogRepository.findAll();
+
+        assertThat(logs).hasSize(11);
+
+        OtpRequestLog blockedLog = logs.getLast();
+
+        assertThat(blockedLog.getEmail()).isEqualTo("user11@gmail.com");
+        assertThat(blockedLog.getIp()).isEqualTo(ipAddress);
+
+        assertThat(blockedLog.getStatus())
+                .isEqualTo(OtpRequestStatusEnum.BLOCKED);
+
+        assertThat(blockedLog.getReason()).isEqualTo("Too many requests from this IP");
+
+        verify(emailService, times(10)).sendOtpEmailAsync(anyString(), anyString());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
